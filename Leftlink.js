b90cbe2d4d43d355b91177184ea7487b1c379ddc
Abhishek <46296076+Abhisshek717@users.noreply.github.com>
2021-01-02 19:09:08 +0530

    added leftlink js and css and rightlink js

---
+++ Leftlink.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Leftlink.css';
+function Leftlink() {
+    return (
+        <div className="leftlink">
+            <div className="title">
+               <span className="title__hello"> <h1>Hello, i'm</h1> </span>
+               <span className="title__abhi"> <h1>Abhishek</h1> </span>                
+            </div>
+            <div className="subtitle">
+                <p className="para">Studying Computer science.  Learning UX & UI 
+                   designing and developing the Web, Android and IOS 
+                   Apps, I code Clean.
+                   Interested in the field of Machine Learning .</p>
+            </div>
+            <div className="left__link">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Leftlink
---
+++ Rightlink.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Rightlink() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Rightlink
