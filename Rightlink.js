41d2d712edf150d1c1b8e51e8d436cae483aa95b
Abhishek <46296076+Abhisshek717@users.noreply.github.com>
2021-01-06 02:37:23 +0530

    added new div's

--- Rightlink.js
+++ Rightlink.js
@@ -1,9 +1,34 @@
 import React from 'react'
-
+import './Rightlink.css'
+import Skills from './Skills'
+import GitHubIcon from '@material-ui/icons/GitHub';
 function Rightlink() {
     return (
-        <div>
-            
+        <div className="right__link">
+           <div className="resume">
+                <h1>Resume</h1>
+           </div>
+           <div className="skills">
+               <div className="heading__skills">Skills</div>
+               <div className="skill">
+                   <div className="skill__item">
+                      <Skills Icon={GitHubIcon} text="React.js"/>
+                      <Skills  Icon={GitHubIcon} text="JavaScript"/>
+                   </div>
+                   
+                      
+                  
+                   <div className="skill__item2">
+                      <Skills  Icon={GitHubIcon} text="Python 3"/>
+                      <Skills  Icon={GitHubIcon} text="Adobe xd"/>
+                   </div>
+                  
+                      
+                   
+                  
+                   
+               </div>
+           </div>
         </div>
     )
 }
