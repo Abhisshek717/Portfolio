9c1b94c8cfa3f2b2df6b2e03f0a283fabd5061f8
Abhishek <46296076+Abhisshek717@users.noreply.github.com>
2021-01-18 10:12:44 +0530

    removed icon prop

--- Skills.js
+++ Skills.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import './Skills.css' 
-function Skills({Icon,text}) {
+function Skills({text}) {
     return (
         <div className="skill__bottom">
-            <Icon/>
+            
             <h1 className="skill__text">{text}</h1>
         </div>
     )
